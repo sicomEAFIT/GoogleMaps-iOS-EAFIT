ffc031cc2410574211c814335a620e5be5267d43
Mateo Olaya Bernal <molayab@iMac-de-Mateo.local>
2013-10-10 23:59:42 -0500

    Se agrego MOServices.m y MOServices.h

--- Mapa EAFIT/MOServices.m
+++ Mapa EAFIT/MOServices.m
@@ -5,6 +5,11 @@
 //  Created by Mateo Olaya Bernal on 10/10/13.
 //  Copyright (c) 2013 Mateo Olaya Bernal. All rights reserved.
 //
+//
+// ACERCA DE:
+// Esta clase se conecta con el servidor remoto y descarga los "Markers" que seran
+// puestos en el mapa de la universidad, asi como otros recursos almacenados.
+//
 
 #import "MOServices.h"
 
